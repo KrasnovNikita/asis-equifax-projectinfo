df26ff3ac488d0e55cae1ab957def913d6276e86
nkrasnov <nkrasnov@datas-tech.com>
2019-06-10 11:23:09 +0300

    Initial commit

---
+++ src/main/java/com/equifax/projectinfo/asisequifaxprojectinfo/AsisEquifaxProjectinfoApplication.java
@@ -0,0 +1,13 @@
+package com.equifax.projectinfo.asisequifaxprojectinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsisEquifaxProjectinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsisEquifaxProjectinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equifax/projectinfo/asisequifaxprojectinfo/AsisEquifaxProjectinfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.equifax.projectinfo.asisequifaxprojectinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsisEquifaxProjectinfoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
